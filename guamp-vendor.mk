54eb77db2ccbf398a6399b2e4973cff69a4f5947
Michael Bestas <mkbestas@lineageos.org>
2024-08-29 18:53:51 +0300

    guamp: Regenerate after extract script changes
    Change-Id: I27dfc4334e8d919ef7cbe2c6d2a0e817aff53900

--- guamp-vendor.mk
+++ guamp-vendor.mk
@@ -234,6 +234,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     libthermalclient
+PRODUCT_COPY_FILES += \
+    vendor/motorola/guamp/proprietary/product/etc/CarrierSettings/carrier_list.pb:$(TARGET_COPY_OUT_PRODUCT)/etc/CarrierSettings/carrier_list.pb \
+    vendor/motorola/guamp/proprietary/product/etc/CarrierSettings/others.pb:$(TARGET_COPY_OUT_PRODUCT)/etc/CarrierSettings/others.pb
 
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay
